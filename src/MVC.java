0fd4b20844ae10b65ba195f064e23006ab270efb
TJSCoding <timoj.schmelzle@gmail.com>
2022-05-04 18:59:00 +0200

    Commit

--- src/MVC.java
+++ src/MVC.java
@@ -10,11 +10,7 @@ public class MVC {
     public static void main(String[] args) {
 
         //new MVCController();
-        Quicksort q = new Quicksort();
-        int[] zuSortieren = {7,2,4,8,6,1,3,5,9};
-        System.out.println(Arrays.toString(zuSortieren));
-        q.quicksort(zuSortieren, 0, 8);
-        System.out.println(Arrays.toString(zuSortieren));
+
         Zutat orange = new Zutat("Orange", 10.00);
         Zutat zitrone = new Zutat("Zitrone", 8.50);
         Zutat zucker = new Zutat("Zucker", 2.50);
